43eb298d708496618eba6762426bc34d9f4ea031
yaolynzc <yaolynzc@126.com>
2017-08-19 14:19:41 +0800

    V1.0.0版本提交

---
+++ README.md
@@ -0,0 +1,23 @@
+## 介绍
+
+基于个人改造后的的torrent爬虫程序(地址：https://github.com/yaolynzc/sp2der)  
+结合个人实际需求开发的简易web程序，主要包括查询、搜索、下载种子等功能
+
+## 用途
+
+仅供个人研究使用
+
+## 开发框架
+
+NodeJS  
+Express      
+MongoDB    
+MySQL   
+
+BootStrap    
+Handlebar  
+jQuery
+
+## License
+MIT  
+
---
+++ public/js/index.js
@@ -0,0 +1,136 @@
+var currentPage = 1;
+var nextpage = true;
+
+$(document).ready(function(){
+  initList();
+
+  $("#btn_search").click(function(){
+    currentPage = 1;
+    getList();
+  });
+
+  $("#btn_newlist").click(function(){
+    $("#name").val('');
+    currentPage = 1;
+    getList();
+  });
+
+  $("#prepage").click(function(){
+    if(currentPage == 1){
+      $("#prepage").parent().removeClass("active").addClass("disabled");
+    }else{
+      $("#nextpage").parent().removeClass("disabled").addClass("active");
+      nextpage = true;
+      currentPage--;
+      getList();
+      if(currentPage == 1){
+        $("#prepage").parent().removeClass("active").addClass("disabled");
+      }
+    }
+  });
+
+  $("#nextpage").click(function(){
+    if(nextpage){
+      $("#prepage").parent().removeClass("disabled").addClass("active");
+      currentPage++;
+      getList();
+    }else{
+      return;
+    }
+  });
+});
+
+// 初始化或最新
+function initList(){
+  var datas = {
+    rad:Math.random()
+  }
+  $.ajax({
+    type: "GET",
+    url: "/search",
+    data: datas,
+    success: function (response) {
+      // console.log(response.dt);
+      if(response.success){
+        var data = response.dt;
+        for(var i in data){
+          var localtime = new Date(data[i].ctime).format('yyyy-MM-dd h:m:s');
+          var template = '<tr>'
+                       + '<td>' + data[i].name + '</td>'
+                       + '<td><a href="/down/' + data[i].file + '">' + data[i].file + '</a></td>'
+                       + '<td>' + localtime + '</td>'
+                       + '</tr>';
+          $("#tdlist").append(template);
+        }
+        if(response.count < 20){
+          $("#nextpage").parent().addClass("disabled");
+          nextpage = false;
+        }
+      }
+    }
+  });
+}
+
+// 搜索
+function getList(){
+  if(currentPage == 1){
+    $("#prepage").parent().removeClass('active').addClass("disabled");
+  }
+  $("#nextpage").parent().removeClass('disabled').addClass("active");
+  nextpage = true;
+
+  var datas = {
+    name:$("#name").val(),
+    page:currentPage,
+    rad:Math.random()
+  }
+  $.ajax({
+    type: "GET",
+    url: "/search",
+    data: datas,
+    success: function (response) {
+      // console.log(response.dt);
+      if(response.success){
+        var data = response.dt;
+        $("#tdlist").html('');
+        for(var i in data){
+          var localtime = new Date(data[i].ctime).format('yyyy-MM-dd h:m:s');
+          var template = '<tr>'
+                       + '<td>' + data[i].name + '</td>'
+                       + '<td><a href="/down/' + data[i].file + '">' + data[i].file + '</a></td>'
+                       + '<td>' + localtime + '</td>'
+                       + '</tr>';
+          $("#tdlist").append(template);
+        }
+
+        if(response.count < 20){
+          $("#nextpage").parent().addClass("disabled");
+          nextpage = false;
+        }
+      }
+    }
+  });
+}
+
+// 格式化时间字符串为yyyy-mm-dd hh:mm:sss
+Date.prototype.format = function(format) {
+  var date = {
+         "M+": this.getMonth() + 1,
+         "d+": this.getDate(),
+         "h+": this.getHours(),
+         "m+": this.getMinutes(),
+         "s+": this.getSeconds(),
+         "q+": Math.floor((this.getMonth() + 3) / 3),
+         "S+": this.getMilliseconds()
+  };
+  if (/(y+)/i.test(format)) {
+         format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in date) {
+         if (new RegExp("(" + k + ")").test(format)) {
+                format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                       ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+         }
+  }
+  return format;
+}
